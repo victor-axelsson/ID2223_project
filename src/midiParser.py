6d9914778fd600f59c01544ab7c43fc24839489a
Victor Axelsson <victor_axelsson@hotmail.com>
2017-12-03 10:29:29 +0100

    Read variable lenght naively

--- src/midiParser.py
+++ src/midiParser.py
@@ -24,24 +24,66 @@ class MidiParser:
 
 	    self.headerChunk = HeaderChunk(chunkID, chunkSize, formatType, numberOfTracks, timeDivision)
 	    self.headerChunk.printIt()
-    
+   
+	def bits(self, bytes):
+		for b in bytes:
+			for i in range(8):
+				yield ((b >> i) & 1, i)
+
 	def _format(self):	
 		with open(self.filepath, "rb") as binary_file:
-		    # Read the whole file at once
-		    data = binary_file.read()
-		    #print(data)
+			# Read the whole file at once
+			data = binary_file.read()
+			#print(data)
 
-		    # Seek position and read N bytes
-		    binary_file.seek(0)  # Go to beginning
+			# Seek position and read N bytes
+			binary_file.seek(0)  # Go to beginning
 
-		    self._formatHeader(binary_file)
+			self._formatHeader(binary_file)
+
+			#Parse the track data
+			chunkID = binary_file.read(4)
+			chunkSize = self.toInt(binary_file.read(4))
+			trackChunk = TrackChunk(chunkID, chunkSize)
+
+			#trackEventData = binary_file.read(chunkSize)
+
+			#Read the delta time
+			read = True
+			vaqString = ""
+			deltaTime = 0
+			while read or bytesRead >= chunkSize:
+				byte = binary_file.read(1)
+				bytesRead += 1
+
+				asString = bin(int.from_bytes(byte, byteorder="big")).strip('0b')
+				vaqString = asString + vaqString
+				if len(asString) < 8 or asString[7] == '0':
+					read = False
+
+			if vaqString == "":
+				deltaTime = 0
+			else:
+				deltaTime = int(vaqString, 2)
+		
 
 
-		    chunkID = binary_file.read(4)
-		    chunkSize = self.toInt(binary_file.read(4))
-		    trackEventData = binary_file.read(chunkSize)
+			byte = binary_file.read(1)
+			for bit, i in self.bits(byte):
+				print("Bit: " + str(bit) + " i: " + str(i))
 
-		    trackChunk = TrackChunk(chunkID, chunkSize, trackEventData)
-		    trackChunk.printIt()
+
+
+			#print(bin(int.from_bytes(binary_file.read(1), byteorder="big")).strip('0b'))
+			#print(bin(int.from_bytes(binary_file.read(1), byteorder="big")).strip('0b'))
+			#print(bin(int.from_bytes(binary_file.read(1), byteorder="big")).strip('0b'))
+			#print(bin(int.from_bytes(binary_file.read(1), byteorder="big")).strip('0b'))
+			#print(print(bin(int.from_bytes(binary_file.read(1), byteorder="big")).strip('0b')))
+			#print(print(bin(int.from_bytes(binary_file.read(1), byteorder="big")).strip('0b')))
+			#print(print(bin(int.from_bytes(binary_file.read(1), byteorder="big")).strip('0b')))
+
+
+			#trackChunk = TrackChunk(chunkID, chunkSize, trackEventData)
+			#trackChunk.printIt()
 
 
--- src/trackChunk.py
+++ src/trackChunk.py
@@ -1,14 +1,18 @@
 class TrackChunk:
 	chunkId = None
 	chunkSize = None
-	trackEventData = None
 
-	def __init__(self, chunkId, chunkSize, trackEventData):
+	def __init__(self, chunkId, chunkSize):
 		self.chunkId = chunkId
 		self.chunkSize = chunkSize
-		self.trackEventData = trackEventData
+		self._format()
+
+	def _format(self):
+		print("F")
 
 	def printIt(self):
 		print("chunkId =>" + str(self.chunkId))
 		print("chunkSize =>" + str(self.chunkSize))
-		print("trackEventData =>" + str(self.trackEventData))
+		#print(bin(int.from_bytes(self.trackEventData, byteorder="big")).strip('0b'))
+		#print(bin(int.from_bytes(self.trackEventData, 	byteorder="little")).strip('0b'))
+		#print("trackEventData =>" + str(self.trackEventData))
