a468c8ade72391517b26e14b6aad06568c7638f0
Victor Axelsson <victor_axelsson@hotmail.com>
2017-12-11 14:15:10 +0100

    fixed a bug with inverted y axis

--- src/drawing/TrackDrawer.py
+++ src/drawing/TrackDrawer.py
@@ -6,19 +6,19 @@ class TrackDrawer:
 		
 		scale = 10000
 		w = int(width / scale)
+		height = 128
 
-		canvas = (w, 128)
+		canvas = (w, height)
 		# init canvas
 		im = Image.new('RGBA', canvas, (255, 255, 255, 255))
 		draw = ImageDraw.Draw(im)
 		#{'x': 27950880, 'y': 60, 'type': 144, 'track': 1}
 		
 		for note in notes:
-
 			x1 = int(note['x'] / scale)
-			y1 = note['y'] + 1
-			x2 = x1 + 5
-			y2 = note['y']
+			y1 = height - note['y']
+			x2 = x1 +5
+			y2 = y1 +1 
 			draw.rectangle([x1, y1, x2, y2], outline=(0, 0, 0, 255))
 
 		im.save('/Users/victoraxelsson/Desktop/data_projects/ML_project/track'+str(note['track'])+'.png')
